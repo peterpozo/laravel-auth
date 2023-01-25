312b1d2ab4553246ec241e3047d5a486e3a35a25
peterpozo <peteranderson.pozo@gmail.com>
2023-01-25 18:21:41 +0100

    first commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        return view('admin.home', compact('user'));
+    }
+}
