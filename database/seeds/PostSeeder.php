00a42fd7f0cdcf06fd201395fc0d21048b76bc76
peterpozo <peteranderson.pozo@gmail.com>
2023-01-30 18:16:04 +0100

    slug update

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Post;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +15,9 @@ class PostSeeder extends Seeder
     {
         for ($i=0; $i < 100; $i++) {
             $title = $faker->words(rand(3,7), true);
+            $title = 'Ciao a Tutti';
             Post::create([
-                'slug' =>Str::slug($title),
+                'slug' =>Post::getslug($title),
                 'title'=>$title,
                 'image' =>'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
                 'content' => $faker ->paragraphs(rand(1, 10), true),
